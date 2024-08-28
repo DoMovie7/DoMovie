810f8c31411db437e3401e0827196960a8dfe9bb
yms <kevinyms@naver.com>
2024-08-28 17:03:54 +0900

    feat: 마이페이지

---
+++ src/main/java/com/red/domovie/controller/MypageController.java
@@ -0,0 +1,22 @@
+package com.red.domovie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class MypageController {
+
+	@GetMapping("/mypage")
+	public String list() {
+		return "/views/user/mypage";
+	}
+	
+	@GetMapping("/mypage/detail")
+	public String update() {
+		return "/views/user/mypageDetail";
+	}
+	
+	
+}
