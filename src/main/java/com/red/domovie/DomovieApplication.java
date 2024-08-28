831f789342d401cb1e76f500b031f7dcefe2a404
dohyun <wlswjqdmq1501@gmail.com>
2024-08-28 10:36:30 +0900

    기본설정

---
+++ src/main/java/com/red/domovie/DomovieApplication.java
@@ -0,0 +1,13 @@
+package com.red.domovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/red/domovie/DomovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.red.domovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
