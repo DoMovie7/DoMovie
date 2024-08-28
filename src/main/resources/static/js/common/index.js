ea0a5ce255bcf5584e34d992b8c87d414012c673
songol <thdgnl200@gamil.com>
2024-08-28 17:03:48 +0900

    feat:헤더 와 레이아ㅜㅅ

---
+++ src/main/resources/static/js/common/index.js
@@ -0,0 +1,83 @@
+//문서의 내용이 모두 로드되면 할일
+document.addEventListener('DOMContentLoaded', function() {
+
+	var slideWrap = document.querySelector('.indexslide-container');
+	var slideContainer = document.querySelector('.indexslide-wrap');
+	var slide = document.querySelectorAll('.indexslide');
+	var navPrev = document.getElementById('prev');
+	var navNext = document.getElementById('next');
+	var currnetIndex = 0;
+	var slidHeight = 0;
+	var slideCount =slide.length;
+
+	//슬라이드의 높이를 확인하여 부모의 높이로 지정하기
+	
+	for(var i=0;i<slideCount;i++){
+		
+		if(slidHeight<slide[i].offsetHeight){
+			
+			slidHeight =slide[i].offsetHeight;
+		}
+		
+	}
+	
+	slideWrap.style.height =slidHeight+'px';
+	slideContainer.style.height =slidHeight+'px';
+	
+	
+	//슬라이드 옆으로 나열하기
+	for(var a=0;a<slideCount; a++){
+		slide[a].style.left = a * 100 +'%';
+	}
+	
+	//슬라이드 이동 함수
+	function goToslide(idx){
+		slideContainer.style.left = -100 * idx + '%';
+		slideContainer.classList.add('animated')
+		
+		currnetIndex=idx;
+		
+	}
+	
+	
+	
+	//버튼을 클릭하면 슬라이드 이동시키기
+	/*
+	navPrev.addEventListener('click',function(){
+		goToslide(currnetIndex -1);
+		console.log(currnetIndex);
+	
+	});
+	
+	
+	navNext.addEventListener('click',function(){
+		goToslide(currnetIndex +1);
+		console.log(currnetIndex);
+		
+	});
+	*/
+	//마지막 슬라이드 후  next를 클릭하면 첫번째 슬라이드 보여주기
+	navPrev.addEventListener('click',function(){
+		if(currnetIndex == 0){
+			goToslide(slideCount-1);
+		}else{
+			goToslide(currnetIndex -1);
+		}
+	});
+	
+	navNext.addEventListener('click',function(){
+		if(currnetIndex == slideCount -1){
+			goToslide(0);
+		}else{
+			goToslide(currnetIndex+1);
+		}
+	});
+
+
+
+
+});
+
+//변수지정
+
+
