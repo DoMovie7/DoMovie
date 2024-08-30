d78f45d24f9c20d77c530ed0aa8a86e242e1d207
dohyun <wlswjqdmq1501@gmail.com>
2024-08-30 10:26:39 +0900

    feat:update

--- src/test/java/com/red/domovie/DomovieApplicationTests.java
+++ src/test/java/com/red/domovie/DomovieApplicationTests.java
@@ -6,7 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.red.domovie.domain.entity.Role;
+import com.red.domovie.domain.entity.TierEntity;
 import com.red.domovie.domain.entity.UserEntity;
+import com.red.domovie.domain.repository.TierEntityRepository;
 import com.red.domovie.domain.repository.UserEntityRepository;
 
 @SpringBootTest
@@ -17,37 +19,35 @@ class DomovieApplicationTests {
 	@Autowired
 	UserEntityRepository mRepository;
 	
+	@Autowired
+	TierEntityRepository tRepository;
+	
+	
 	//@Test
-	void singIn() {
-		// 일반 사용자 생성
-        UserEntity user = UserEntity.builder()
-                .userName("일반 사용자")
-                .nickName("유저1")
-                .email("user@test.com")
-                .phoneNumber("01012345678")
-                .password(passwordEncoder.encode("1234"))
-                .birthDate("1990-01-01")
-                .status(1L)
-                .build()
-                .addRole(Role.USER);
+	void signIn() {
+	    // 기본 티어 생성 (ID: 1)
+	    TierEntity defaultTier = TierEntity.builder()
+	        .minPostCount(0)
+	        .maxPostCount(10)
+	        .build();
+	    tRepository.save(defaultTier);
+	    
+	    // 일반 사용자 생성
+	    UserEntity user = UserEntity.builder()
+	        .userName("일반 사용자")
+	        .nickName("유저1")
+	        .email("user@test.com")
+	        .phoneNumber("01012345678")
+	        .password(passwordEncoder.encode("1234"))
+	        .birthDate("1990-01-01")
+	        .status(1L)
+	        .tier(defaultTier)  // 기본 티어 설정
+	        .build()
+	        .addRole(Role.USER);
 
-        mRepository.save(user);
-
-        // 관리자 생성
-        UserEntity admin = UserEntity.builder()
-                .userName("관리자")
-                .nickName("어드민1")
-                .email("admin@test.com")
-                .phoneNumber("01087654321")
-                .password(passwordEncoder.encode("1234"))
-                .birthDate("1985-12-31")
-                .status(1L)
-                .build()
-                .addRole(Role.USER)
-                .addRole(Role.ADMIN);
-
-        mRepository.save(admin);
-
+	    // 저장 및 검증 로직
+	    mRepository.save(user);
+	    
         System.out.println("테스트 데이터가 성공적으로 초기화되었습니다.");
 	}
 }
