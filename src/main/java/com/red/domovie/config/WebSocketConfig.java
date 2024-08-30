d4162963bfc27ec277af79b1bd89e0f0c472e897
kdy <sksl6134@gmail.com>
2024-08-30 17:37:40 +0900

    feat: chatbot backend

--- src/main/java/com/red/domovie/config/WebSocketConfig.java
+++ src/main/java/com/red/domovie/config/WebSocketConfig.java
@@ -10,22 +10,33 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	
+	// STOMP 프로토콜을 사용하여 웹소켓 엔드포인트를 등록
+	// client -> "var socket=new SockJS("/chatbot")" 서버에 웹소켓 연결 시도
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		
-		// "/bookBot" 을 엔드포인트로 등록
-		registry.addEndpoint("/chatbot").withSockJS();
+		registry.addEndpoint("/chatbot").withSockJS(); //withSockJS() : 웹소켓 지원하지 않으면 sockJS 사용
 		
-		// 클라이언트 쪽에서 자바스크립트를 통해 웹소켓을 연결할 때
-		// var socket=new SockJS("/chatbot") 과 같이 사용한다
 	}
 
+	//메세지 브로커의 구성 설정
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		
-		//사용자->서버 메세지를 전송할때
-		// 클라이언트가 서버로 메시지를 보낼 때 "/message/경로" 형식으로 보낸다
+		// "/message"로 시작하는 메시지는 @MessageMapping이 붙은 메서드로 라우팅
 		registry.setApplicationDestinationPrefixes("/message");
+		
+		// 메시지 브로커를 RabbitMQ로 설정
+        // "/queue" 접두사가 붙은 메시지는 RabbitMQ로 라우팅
+        /*
+		registry.enableStompBrokerRelay("/queue")
+                .setRelayHost("localhost")  // RabbitMQ 호스트 (기본적으로 로컬)
+                .setRelayPort(61613)        // RabbitMQ STOMP 포트 (기본적으로 61613)
+                .setClientLogin("guest")    // RabbitMQ 로그인 ID
+                .setClientPasscode("guest") // RabbitMQ 로그인 패스워드
+                .setSystemLogin("guest")    // 시스템 로그인 ID (관리자용)
+                .setSystemPasscode("guest");// 시스템 로그인 패스워드 (관리자용)
+       */
 	}
 
 }
