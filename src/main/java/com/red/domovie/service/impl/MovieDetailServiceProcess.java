054bac83f49cadb2a142107a8083aa143de45d2e
songol <thdgnl200@gamil.com>
2024-09-02 17:46:20 +0900

    feat:json응답 dto에 매핑

--- src/main/java/com/red/domovie/service/impl/MovieDetailServiceProcess.java
+++ src/main/java/com/red/domovie/service/impl/MovieDetailServiceProcess.java
@@ -1,11 +1,17 @@
 package com.red.domovie.service.impl;
 
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.red.domovie.common.util.KmdbMovieUtil;
+import com.red.domovie.domain.dto.movieDetail.GetMovieDetailDTO;
 import com.red.domovie.service.MovieDetailService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,8 +28,19 @@ public class MovieDetailServiceProcess implements MovieDetailService {
 		ResponseEntity<JsonNode> response = kmdbMovieUtil.getDtailMovie(movieID);
 		JsonNode responseBody = response.getBody();
         System.out.println("API Response: " + responseBody);
-		
-		
+        if(response.getStatusCode() == HttpStatus.OK) {
+        	GetMovieDetailDTO movieDetail=GetMovieDetailDTO.toDTO(responseBody);
+			 model.addAttribute("movieDetail", movieDetail);
+			 
+			 System.out.println("Movie Detail DTO:");
+		     System.out.println(movieDetail.toString());
+			
+	        	
+	        }
+	
 	}
 
+	
+	
+	
 }
