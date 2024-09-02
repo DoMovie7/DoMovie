32ac40b402df9faca2fb21a10e5f1f630d3c3538
kdy <sksl6134@gmail.com>
2024-09-02 15:31:27 +0900

    feat: chatbot

--- src/main/java/com/red/domovie/controller/BotController.java
+++ src/main/java/com/red/domovie/controller/BotController.java
@@ -16,14 +16,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.red.domovie.service.BotService;
+import com.red.domovie.service.CategoryService;
 import com.red.domovie.service.ChatService;
+import com.red.domovie.service.OpenaiService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.red.domovie.domain.dto.ResponseDTO;
 import com.red.domovie.domain.dto.bot.ChatRoomDTO;
+import com.red.domovie.domain.dto.bot.FAQDTO;
 import com.red.domovie.domain.dto.bot.QuestionDTO;
 import com.red.domovie.security.CustomUserDetails;
 
@@ -40,9 +44,11 @@ public class BotController {
 	private String routingKey;
 	
 	private final RabbitTemplate rabbitTemplate;
+	private final ObjectMapper objectMapper;
 	private final BotService botService;
 	private final ChatService chatService;
-	private final ObjectMapper objectMapper;
+	private final CategoryService categoryService;
+	private final OpenaiService aiService;
 	
 	//내부적으로 STOMP의 프로토콜을 사용하여 메세지를 전송
 	//@SendTo 어노테이션을 처리하는 구현 객체
@@ -62,9 +68,19 @@ public class BotController {
         String jsonResponse = objectMapper.writeValueAsString(responseDTO);
 		
         System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>"+jsonResponse);
-		rabbitTemplate.convertAndSend(exchange, routingKey, jsonResponse);
+		//rabbitTemplate.convertAndSend(exchange, routingKey, jsonResponse);
 		
 	}
+	
+	
+	//자주 묻는 질문
+	@GetMapping("/api/categories")
+	@ResponseBody
+    public List<FAQDTO> getAllCategories() {
+		System.out.println(categoryService.getAllFaqCategories());
+        return categoryService.getAllFaqCategories();
+    }
+	
 	
 	//상담사와 채팅
 	@MessageMapping("/agent")
@@ -72,7 +88,19 @@ public class BotController {
 		
 		System.out.println(">>>채팅 문의 :"+dto);
 		String key = dto.getKey();
-		String responseMessage = botService.questionProcess(dto);
+		String responseMessage = "아직 상담사와 연결되지 않았습니다. 잠시만 기다려주세요.";
+		
+		messagingTemplate.convertAndSend("/topic/bot/"+key, responseMessage);
+		
+	}
+	
+	//영화추천
+	@MessageMapping("/openai")
+	public void openai(QuestionDTO dto) {
+		
+		System.out.println(">>>영화추천 :"+dto);
+		String key = dto.getKey();
+		String responseMessage = null;
 		
 		messagingTemplate.convertAndSend("/topic/bot/"+key, responseMessage);
 		
---
+++ src/main/java/com/red/domovie/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.red.domovie.service;
+
+import java.util.List;
+
+import com.red.domovie.domain.dto.bot.FAQDTO;
+
+public interface CategoryService {
+
+	List<FAQDTO> getAllFaqCategories();
+
+}
