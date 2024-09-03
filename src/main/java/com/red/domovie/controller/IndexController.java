adc3afdaef9d3fbf9d97870d4c51b65abcc61c81
yhr <yhr0841@gmail.com>
2024-09-03 14:43:58 +0900

    index12

--- src/main/java/com/red/domovie/controller/IndexController.java
+++ src/main/java/com/red/domovie/controller/IndexController.java
@@ -1,10 +1,13 @@
 package com.red.domovie.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
+import com.red.domovie.domain.dto.movie.KmdbMovieDTO;
 import com.red.domovie.service.MovieApiService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,7 +23,7 @@ public class IndexController {
         // 영화 정보 가져오기
         movieApiService.getNewMovies(model);
         
-        return "views/index-movie-list";  // 타임리프 템플릿 이름 반환
+        return "views/index/index-movie-list";  // 타임리프 템플릿 이름 반환
     }
     
     
@@ -28,21 +31,21 @@ public class IndexController {
     public String getBoxOffice(Model model) {
     
     	movieApiService.getBoxOffice(model);
-        return "views/index-box-movie-list";
+        return "views/index/index-box-movie-list";
     }
     
     @GetMapping("/movies/horror")
     public String getHorrorMovies(Model model) {
       
     	movieApiService.getHorrorMovies(model);
-        return "views/index-movie-list";
+        return "views/index/index-movie-list";
     }
     
     @GetMapping("/movies/animation")
     public String getAnimationMovies(Model model) {
       
     	movieApiService.getAnimationMovies(model);
-        return "views/index-movie-list";
+        return "views/index/index-movie-list";
     }
     
     //getUpcomingMovies
@@ -50,7 +53,17 @@ public class IndexController {
     public String getUpcomingMovies(Model model) {
       
     	movieApiService.getUpcomingMovies(model);
-        return "views/index-movie-list";
+        return "views/index/index-movie-list";
     }
+    
+    @GetMapping("/movies/search")
+    public String getSearchMovies(@RequestParam("keyword") String keyword, Model model) {
+        // KMDB API를 호출하여 검색 결과를 가져옵니다.
+        List<KmdbMovieDTO> movies = movieApiService.searchMovies(keyword);
+        model.addAttribute("list", movies);
+        model.addAttribute("keyword", keyword);  // 검색어를 모델에 추가하여 폼에 표시
+        return "views/index/search-movie"; // Thymeleaf 템플릿 파일
+    }
+
     
 }
