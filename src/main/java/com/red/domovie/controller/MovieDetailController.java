5285e72cf64ae7167124c59d41eecec8b3fdead7
PDH3390\aabb3 <qkrehgus303>
2024-09-04 20:46:19 +0900

    feat:update

--- src/main/java/com/red/domovie/controller/MovieDetailController.java
+++ src/main/java/com/red/domovie/controller/MovieDetailController.java
@@ -13,6 +13,7 @@ import com.red.domovie.service.MovieDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RequiredArgsConstructor
