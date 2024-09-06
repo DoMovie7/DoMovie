e4687e14642afe69b3f31e152341f40140a10006
doyoung0013 <163370991+doyoung0013@users.noreply.github.com>
2024-09-06 17:14:43 +0900

    feat: chatbot finish

--- src/main/java/com/red/domovie/service/impl/ChatServiceProcess.java
+++ src/main/java/com/red/domovie/service/impl/ChatServiceProcess.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import org.springframework.stereotype.Service;
 
 import com.red.domovie.domain.dto.bot.QuestionDTO;
+import com.red.domovie.domain.dto.chat.AnswerDTO;
+import com.red.domovie.domain.dto.chat.ChatRoomDTO;
+import com.red.domovie.domain.dto.chat.ChattingDTO;
 import com.red.domovie.domain.dto.chat.ChattingRoomDTO;
 import com.red.domovie.domain.dto.chat.PageDTO;
 import com.red.domovie.domain.mapper.RoomMapper;
@@ -48,6 +51,34 @@ public class ChatServiceProcess implements ChatService{
         List<ChattingRoomDTO> content = roomMapper.findAllRooms(params);
         long totalElements = roomMapper.countAllRooms();
         return new PageDTO(page, size, totalElements, content);
+	}
+	
+	@Override
+	public void saveChatQuestion(QuestionDTO dto) {
+		roomMapper.saveChatQuestion(dto);
+		
+	}
+
+	@Override
+	public void saveChatAnswer(AnswerDTO dto) {
+		roomMapper.saveChatAnswer(dto);
+		
+	}
+
+	@Override
+	public List<ChattingDTO> findChatByRoomId(String roomId) {
+		return roomMapper.findChatByRoomId(roomId);
+	}
+
+	@Override
+	public void timeUpdateRoom(String key) {
+		roomMapper.updateTime(key);
+		
+	}
+
+	@Override
+	public ChatRoomDTO findRoomInfo(String roomId) {
+		return roomMapper.findByRoomInfo(roomId);
 	}
 
 }
