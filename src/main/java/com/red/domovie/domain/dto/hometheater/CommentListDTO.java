8c2890f2b5eb4207d4570910d16cc156e0401dfb
parkdohyun <wlswjqdmq1501@gmail.com>
2024-09-09 11:40:50 +0900

    완성본

--- src/main/java/com/red/domovie/domain/dto/hometheater/CommentListDTO.java
+++ src/main/java/com/red/domovie/domain/dto/hometheater/CommentListDTO.java
@@ -2,10 +2,12 @@ package com.red.domovie.domain.dto.hometheater;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CommentListDTO {
     private Long id;
